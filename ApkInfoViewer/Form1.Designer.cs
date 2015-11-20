b413d7b000d9a2c2edbedb5011554f0bbc50a3a2
xiaozi7912 <xiaozi7912@gmail.com>
2015-11-21 01:04:13 +0800

    [Title] Feature list device and install apk use adb command [Symptom] NA [Root Cause] NA [Solution] NA

--- ApkInfoViewer/Form1.Designer.cs
+++ ApkInfoViewer/Form1.Designer.cs
@@ -37,20 +37,29 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 13);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(279, 22);
+            this.textBox2.Size = new System.Drawing.Size(376, 25);
             this.textBox2.TabIndex = 1;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(297, 12);
+            this.button2.AutoSize = true;
+            this.button2.Font = new System.Drawing.Font("Consolas", 9F);
+            this.button2.Location = new System.Drawing.Point(399, 10);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 22);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 3;
             this.button2.Text = "Folder";
             this.button2.UseVisualStyleBackColor = true;
@@ -58,69 +67,128 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(88, 40);
+            this.textBox3.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(139, 50);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(284, 22);
+            this.textBox3.Size = new System.Drawing.Size(360, 25);
             this.textBox3.TabIndex = 5;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(88, 68);
+            this.textBox4.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(139, 85);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(284, 22);
+            this.textBox4.Size = new System.Drawing.Size(360, 25);
             this.textBox4.TabIndex = 6;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(88, 96);
+            this.textBox5.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(139, 120);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(284, 22);
+            this.textBox5.Size = new System.Drawing.Size(360, 25);
             this.textBox5.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("新細明體", 9F);
-            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 12);
+            this.label1.Size = new System.Drawing.Size(96, 18);
             this.label1.TabIndex = 8;
             this.label1.Text = "PackageName";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 88);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 12);
+            this.label2.Size = new System.Drawing.Size(96, 18);
             this.label2.TabIndex = 9;
             this.label2.Text = "VersionCode";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 99);
+            this.label3.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 123);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 12);
+            this.label3.Size = new System.Drawing.Size(96, 18);
             this.label3.TabIndex = 10;
             this.label3.Text = "VersionName";
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(12, 124);
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(12, 153);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(360, 124);
+            this.listBox1.Size = new System.Drawing.Size(487, 148);
             this.listBox1.TabIndex = 11;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(339, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 28);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Refresh Devices";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Location = new System.Drawing.Point(12, 342);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox2.Size = new System.Drawing.Size(487, 148);
+            this.listBox2.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(12, 308);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 28);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Install APK";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 496);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(487, 23);
+            this.progressBar1.TabIndex = 15;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.ClientSize = new System.Drawing.Size(512, 533);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -130,6 +198,7 @@
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "APKInfoViewer";
@@ -149,6 +218,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- ApkInfoViewer/Form1.cs
+++ ApkInfoViewer/Form1.cs
@@ -1,26 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ApkInfoViewer
 {
     public partial class Form1 : Form
     {
+        private String ADB_PATH = Directory.GetCurrentDirectory() + @"\adb.exe";
         private String AAPT_PATH = Directory.GetCurrentDirectory() + @"\aapt.exe";
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.DisplayMember = "Name";
+            listBox1.ValueMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            refreshDeviceList();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -47,6 +52,40 @@ namespace ApkInfoViewer
                     }
                 }
             }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int selectedApkCount = listBox1.SelectedItems.Count;
+            int selectedDeviceCount = listBox2.SelectedItems.Count;
+
+            if (selectedApkCount != 0 && selectedDeviceCount != 0)
+            {
+                String selectedFilePath = (listBox1.SelectedItem as FileInfo).FullName;
+                progressBar1.Maximum = selectedDeviceCount;
+                progressBar1.Value = 0;
+                Console.WriteLine("selectedFilePath : " + selectedFilePath);
+                foreach (object selectedItem in listBox2.SelectedItems)
+                {
+                    Console.WriteLine("selectedItem : " + selectedItem);
+                    new Thread(() =>
+                    {
+                        installAPK(selectedItem.ToString(), selectedFilePath);
+                    }).Start();
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FileInfo selectedItem = listBox1.SelectedItem as FileInfo;
+            String filePath = selectedItem.FullName;
+            Console.WriteLine("filePath : " + filePath);
+            showSelectedInfo(filePath);
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+
         }
 
         private void showSelectedInfo(String filePath)
@@ -79,24 +118,80 @@ namespace ApkInfoViewer
                     Console.WriteLine(regex.IsMatch(readLine));
                 }
             }
-            catch
+            catch (Exception e)
             {
 
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void refreshDeviceList()
         {
-            listBox1.DisplayMember = "Name";
-            listBox1.ValueMember = "FullName";
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.FileName = ADB_PATH;
+            startInfo.Arguments = String.Format("devices");
+            listBox2.Items.Clear();
+
+            try
+            {
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    Regex regex = new Regex(@"([\w\.-:]+)\tdevice", RegexOptions.IgnoreCase);
+                    while (!exeProcess.StandardOutput.EndOfStream)
+                    {
+                        String readLine = exeProcess.StandardOutput.ReadLine();
+                        Console.WriteLine(readLine);
+                        if (regex.IsMatch(readLine))
+                        {
+                            Match match = regex.Match(readLine);
+                            listBox2.Items.Add(match.Groups[1]);
+                            Console.WriteLine(match.Groups[0]);
+                            Console.WriteLine(match.Groups[1]);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void installAPK(String device, String filePath)
         {
-            FileInfo selectedItem = listBox1.SelectedItem as FileInfo;
-            String filePath = selectedItem.FullName;
-            Console.WriteLine("filePath : " + filePath);
-            showSelectedInfo(filePath);
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.FileName = ADB_PATH;
+            startInfo.Arguments = String.Format("-s {0} install -r {1}", device, filePath);
+
+            try
+            {
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    while (!exeProcess.StandardOutput.EndOfStream)
+                    {
+                        String readLine = exeProcess.StandardOutput.ReadLine();
+                        Console.WriteLine(readLine);
+                        if (readLine.ToLower().Equals("success"))
+                        {
+                            progressBar1.Invoke(new Action(() =>
+                            {
+                                progressBar1.Value++;
+                            }));
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
         }
     }
 }
